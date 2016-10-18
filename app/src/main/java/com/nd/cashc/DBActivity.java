1cb0959d697390a956cb45187c99bd43a2a172b4
andrew.simakov <andrake16@gmail.com>
2016-10-18 17:01:18 +0700

    minor

--- app/src/main/java/com/nd/cashc/DBActivity.java
+++ app/src/main/java/com/nd/cashc/DBActivity.java
@@ -1,13 +1,35 @@
 package com.nd.cashc;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DBActivity extends AppCompatActivity {
 
+    private ViewHolder viewHolder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db);
+        viewHolder = new ViewHolder();
+
+        viewHolder.btnAddRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),addProductActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private class ViewHolder {
+        Button btnAddRecord = (Button) findViewById(R.id.buttonAddRecord);
+        Button createDB = (Button) findViewById(R.id.buttonCreateDB);
+        Button viewRecords = (Button) findViewById(R.id.buttonViewRecords);
     }
 }
--- app/src/main/java/com/nd/cashc/addProductActivity.java
+++ app/src/main/java/com/nd/cashc/addProductActivity.java
@@ -2,12 +2,37 @@ package com.nd.cashc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import Database.*;
 
 public class addProductActivity extends AppCompatActivity {
+
+    private ViewHolder viewHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+        viewHolder = new ViewHolder();
+
+        viewHolder.addRecBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAddRecord();
+            }
+        });
+    }
+
+    private void onAddRecord(){
+        com.nd.cashc.Database.CursorSystem.CC_DatabaseHelper
+    }
+
+    private class ViewHolder {
+        EditText productName = (EditText) findViewById(R.id.editText_Product);
+        EditText amount = (EditText) findViewById(R.id.editTextAmount);
+        EditText price = (EditText) findViewById(R.id.editTextPrice);
+        Button addRecBtn = (Button) findViewById(R.id.buttonAddRecord);
     }
 }
